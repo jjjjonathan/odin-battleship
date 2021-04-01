035406ca915511fc48fc17c33cdd929da6cc03d4
jjjjonathan <hornathanjon@gmail.com>
2021-04-01 16:28:14 -0500

    webpack setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import './style.css';
+
+const header = $('<h1></h1>').text('Hellooooo');
+
+$('body').append(header);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'HELLOOOO',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    open: 'Google Chrome',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
